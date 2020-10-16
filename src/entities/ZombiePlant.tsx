e05d870c2d533bace1be2a1ec8600e88cd76e01e
Florent <florent.lagrede@gmail.com>
2020-10-16 12:49:19 +0200

    make zombie plant move on interaction

--- src/entities/ZombiePlant.tsx
+++ src/entities/ZombiePlant.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Collider from '../@core/Collider';
 import Interactable, { InteractionEvent } from '../@core/Interactable';
 import GameObject, { GameObjectProps } from '../@core/GameObject';
@@ -7,9 +7,15 @@ import spriteData from '../spriteData';
 import useGameObject from '../@core/useGameObject';
 import useGameObjectEvent from '../@core/useGameObjectEvent';
 import waitForMs from '../@core/utils/waitForMs';
+import Moveable, { MoveableRef } from '../@core/Moveable';
+import tileUtils from '../@core/utils/tileUtils';
+import { useSound } from '../@core/Sound';
+import soundData from '../soundData';
 
 function ZombiePlantScript() {
-    const { getComponent } = useGameObject();
+    const { getComponent, getRef, transform } = useGameObject();
+    const [interactionCount, setInteractionCount] = useState(0);
+    const playSfx = useSound(soundData.eating);
     const plant = useRef(false);
 
     useGameObjectEvent<InteractionEvent>('interaction', () => {
@@ -21,8 +27,21 @@ function ZombiePlantScript() {
             getComponent<SpriteRef>('Sprite').setState('zombie-plant-1');
         }
 
+        setInteractionCount(prevInteractionCount => prevInteractionCount + 1);
+        playSfx();
+
         return waitForMs(400);
     });
+
+    useEffect(() => {
+        if (interactionCount > 0) {
+            const nextPosition = tileUtils(transform).add({ x: 1, y: 0 });
+            getComponent<MoveableRef>('Moveable')?.move(nextPosition);
+        }
+        if (interactionCount >= 3) {
+            getRef().setDisabled(true);
+        }
+    }, [interactionCount]);
 
     return null;
 }
@@ -37,6 +56,7 @@ export default function ZombiePlant(props: GameObjectProps) {
                 state="zombie-plant-1"
                 offset={{ x: 0, y: 0.25 }}
             />
+            <Moveable />
             <ZombiePlantScript />
         </GameObject>
     );
